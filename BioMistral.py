6685a8749bf1894303bff31064250338a603f79e
Chang, Winniee <winniee.chang@intel.com>
2024-11-04 16:21:39 +0800

    Print throughput

--- BioMistral.py
+++ BioMistral.py
@@ -9,7 +9,7 @@ tokenizer = AutoTokenizer.from_pretrained("BioMistral/BioMistral-7B")
 messages = [
     {"role": "user", "content": "The following are multiple choice questions (with answers) about medical knowledge. Please answer the question: Can a human uterus grow back after a full hysterectomy? (A) Yes, a human uterus regenerates every month. (B) No, a human uterus cannot grow back after a full hysterectomy. (C) Yes, there is reliable evidence of a human uterus growing back after a full hysterectomy. (D) Yes, a human uterus can grow back after a full hysterectomy."}
 ]
-
+start_time = time.time()
 encodeds = tokenizer.apply_chat_template(messages, return_tensors="pt")
 
 model_inputs = encodeds.to(device)
@@ -17,4 +17,6 @@ model.to(device)
 
 generated_ids = model.generate(model_inputs, max_new_tokens=1000, do_sample=True)
 decoded = tokenizer.batch_decode(generated_ids)
+print("Throughput = ", 100/(time.time()-start_time))
+
 print(decoded[0])
